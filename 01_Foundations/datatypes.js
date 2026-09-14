9afb31d804be481d3c2390d2793219f5d6993717
ASIM04 <fullstackdeveloper348@gmail.com>
2026-09-14 06:23:14 +0000

    primptive and non primptive data types in the dynamic prog. language js

--- 01_Foundations/datatypes.js
+++ 01_Foundations/datatypes.js
@@ -1,7 +1,27 @@
-let taste = 15
-console.log(typeof taste);
-taste = String(taste)
-console.log(typeof taste);
+// let taste = 15
+// console.log(typeof taste);
+// taste = String(taste)
+// console.log(typeof taste);
+// console.log(2 > 1);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// always use clean code instead of this types of unclear comparisions
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// data types 
+// -primptive
+// Number,string,boolean,undefined, null,symbol
+// -non primptive (reference)
+// Array,functons,object
+
+
+
+
+
 
 
 
